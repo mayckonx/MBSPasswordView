003130dd656b06e750935ebdbb7a38bc22e1fc41
Mayckon <mayckonxp@gmail.com>
2019-03-03 09:42:17 +0100

    Compiled sources updated

---
+++ MBSPasswordView/Classes/ext/UIViewController+ext.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewController+ext.swift
+//  MBSPasswordView
+//
+//  Created by Mayckon Barbosa da Silva on 3/2/19.
+//  Copyright © 2019 Mayckon Barbosa da Silva. All rights reserved.
+//
+
+import Foundation
+
+// Workaround to make those method optionals
+// Perhaps the user wants to use only one of them
+extension MBSPasswordDelegate where Self: UIViewController {
+    func passwordFromBiometrics(_ result: Result<[String]>) {}
+    func password(_ result: [String]) {}
+}
